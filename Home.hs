894a3d1e6d31c0a2998b7f72b9dd6627da0fda86
romefeller <algarcia@usp.br>
2017-05-02 22:14:01 +0000

    Acertos

--- Home.hs
+++ Home.hs
@@ -5,17 +5,11 @@ module Home where
 
 import Foundation
 import Yesod
-import Text.Julius
 
 getHomeR :: Handler Html
 getHomeR = defaultLayout $ do
     setTitle "Minimal Multifile"
-    toWidget $(juliusFile "templates/Cliente.julius")
-    toWidget [julius|
-       var node = document.getElementById('elm-cli');
-       var app = Elm.Cliente.embed(node);
-    |]
     [whamlet|
-        <div id="elm-cli">
-            
+        <p>
+            Ola mundo!
     |]
